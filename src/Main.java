aaa3263beacb82509d5d33cb411f12c9c11b2f15
Eggry Ran <eggry@live.cn>
2019-01-09 19:40:29 +0800

    main函数+结果文件

--- src/Main.java
+++ src/Main.java
@@ -12,38 +12,35 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, MidiFormatError {
 		Scanner in=new Scanner(System.in);
-		System.out.println("Enter path of Midi File:");
-		////MarkovChain mc=new MarkovChain();
-		//String inputStr;
-		MidiInputParser mip=new MidiInputParser(new DataInputStream(new FileInputStream(in.nextLine())));
-		mip.prase();
-//		while(false) {
-//			
-//			
-//			try {
-//				
-//				mip.prase();
-//				mc.addList(mip.parseResult);
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				System.err.println("Can't find file:"+inputStr);
-//				e.printStackTrace();
-//			} catch (MidiFormatError e) {
-//				// TODO Auto-generated catch block
-//				System.err.println("File format Err:"+inputStr);
-//				e.printStackTrace();
-//			}
-//			
-//			
-//			
-//		}
-		//System.out.println("totalCount:"+mc.totalMeasureCount+"uniqueCount:"+mc.uniqueMeasureCount());
-		//mc.generate();
+		System.out.println("Enter path of Midi File:(-1 to stop)");
+		MarkovChain mc=new MarkovChain();
+		String inputStr;
+		while(in.hasNextLine()&&!(inputStr=in.nextLine().trim()).equals("-1")) {
+			try {
+				MidiInputParser mip=new MidiInputParser(new DataInputStream(new FileInputStream(inputStr)));
+				mip.prase();
+				mc.addList(mip.parseResult);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.err.println("Can't find file:"+inputStr);
+				e.printStackTrace();
+			} catch (MidiFormatError e) {
+				// TODO Auto-generated catch block
+				System.err.println("File format Err:"+inputStr);
+				e.printStackTrace();
+			}
+
+		}
+		System.out.println("totalCount:"+mc.totalMeasureCount+"uniqueCount:"+mc.uniqueMeasureCount()+"Ave:"+mc.totalMeasureCount/mc.totalListCount);
+		mc.generate();
+		System.out.println(mc.result);
+		System.out.println("MeasureCnt:"+mc.result.size());
+		
 		System.out.println("Enter path of output Midi File:");
 		try {
 			DataOutputStream dos=new DataOutputStream(new BufferedOutputStream(new FileOutputStream(in.nextLine().trim())));
 			MidiOutputParser mop=new MidiOutputParser(dos);
-			mop.writeFile(mip.parseResult);
+			mop.writeFile(mc.result);
 			dos.close();
 			System.out.println("writeOK");
 		} catch (IOException e) {
@@ -51,10 +48,7 @@ public class Main {
 			System.err.println("Can't write file:");
 			e.printStackTrace();
 		}
-		System.out.println(mip.parseResult);
 		in.close();
-		
-		
-		
+
 	}
 }
