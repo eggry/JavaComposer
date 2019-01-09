c85d8abd76fa011eae3fd9073bb1a71caff404fd
Eggry Ran <eggry@live.cn>
2019-01-09 16:00:33 +0800

    整合、简单写了一些Main函数以测试MidiOutPut

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+//import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
@@ -10,40 +10,45 @@ import java.util.Scanner;
 public class Main {
 
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException, MidiFormatError {
 		Scanner in=new Scanner(System.in);
-		System.out.println("Enter path of Midi File:(-1 to stop)");
-		MarkovChain mc=new MarkovChain();
-		String inputStr;
-		while(in.hasNextLine()&&!(inputStr=in.nextLine().trim()).equals("-1")) {
-			
-			
-			try {
-				MidiInputParser mip=new MidiInputParser(new DataInputStream(new FileInputStream(inputStr)));
-				mip.prase();
-				mc.addList(mip.parseResult);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				System.err.println("Can't find file:"+inputStr);
-				e.printStackTrace();
-			} catch (MidiFormatError e) {
-				// TODO Auto-generated catch block
-				System.err.println("File format Err:"+inputStr);
-				e.printStackTrace();
-			}
-			
-			
-			
-		}
-		System.out.println("totalCount:"+mc.totalMeasureCount+"uniqueCount:"+mc.uniqueMeasureCount());
-		mc.generate();
-		System.out.println("Enter path of output Midi File:(-1 to stop)");
+		System.out.println("Enter path of Midi File:");
+		////MarkovChain mc=new MarkovChain();
+		//String inputStr;
+		MidiInputParser mip=new MidiInputParser(new DataInputStream(new FileInputStream(in.nextLine())));
+		mip.prase();
+//		while(false) {
+//			
+//			
+//			try {
+//				
+//				mip.prase();
+//				mc.addList(mip.parseResult);
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				System.err.println("Can't find file:"+inputStr);
+//				e.printStackTrace();
+//			} catch (MidiFormatError e) {
+//				// TODO Auto-generated catch block
+//				System.err.println("File format Err:"+inputStr);
+//				e.printStackTrace();
+//			}
+//			
+//			
+//			
+//		}
+		//System.out.println("totalCount:"+mc.totalMeasureCount+"uniqueCount:"+mc.uniqueMeasureCount());
+		//mc.generate();
+		System.out.println("Enter path of output Midi File:");
 		try {
-			MidiOutputParser mop=new MidiOutputParser(new DataOutputStream(new BufferedOutputStream(new FileOutputStream(in.nextLine().trim()))));
-			mop.writeFile(mc.result);
+			DataOutputStream dos=new DataOutputStream(new BufferedOutputStream(new FileOutputStream(in.nextLine().trim())));
+			MidiOutputParser mop=new MidiOutputParser(dos);
+			mop.writeFile(mip.parseResult);
+			dos.close();
+			System.out.println("writeOK");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
-			System.err.println("Can't write file:"+inputStr);
+			System.err.println("Can't write file:");
 			e.printStackTrace();
 		}
 		
